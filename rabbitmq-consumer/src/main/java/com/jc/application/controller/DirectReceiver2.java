7d02fa46a5fb589d284fd6315ed0496d65e49699
jc <156196575@qq.com>
2021-01-08 11:12:54 +0800

    练习提交

---
+++ rabbitmq-consumer/src/main/java/com/jc/application/controller/DirectReceiver2.java
@@ -0,0 +1,30 @@
+/*
+package com.jc.application.controller;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+
+public class DirectReceiver2{
+
+
+
+    @RabbitHandler
+
+    public void process(Map testMessage) {
+
+        System.out.println("DirectReceiver2消费者收到消息  : " + testMessage.toString());
+
+    }
+
+
+
+}
+*/
